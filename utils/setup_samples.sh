9442dfb54e6c4924cff0038518778200b4ac7c8a
Maximilian Burkart <maxiburkart@gmail.com>
2021-03-10 09:02:23 +0100

    Switch to FFs v3 and change name of era variable to omit overwritting of variable.

--- utils/setup_samples.sh
+++ utils/setup_samples.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 set -e
 
-ERA=$1
+E=$1
 
 #### ERA specific part. If a sample is not available comment it out here.
 # Samples Run2016
 basedir="/ceph/htautau/Run2Legacy_MSSM"
 ARTUS_OUTPUTS_2016="$basedir/2016/ntuples/"
 SVFit_Friends_2016="$basedir/2016/friends/SVFit/"
-FF_Friends_2016="$basedir/2016/friends/FakeFactors/"
+FF_Friends_2016="$basedir/2016/friends/FakeFactors_v3/"
 NLOReweighting_Friends_2016="$basedir/2016/friends/NLOReweighting/"
 
 # Samples Run2017
 ARTUS_OUTPUTS_2017="$basedir/2017/ntuples/"
 SVFit_Friends_2017="$basedir/2017/friends/SVFit/"
-FF_Friends_2017="$basedir/2017/friends/FakeFactors/"
+FF_Friends_2017="$basedir/2017/friends/FakeFactors_v3/"
 NLOReweighting_Friends_2017="$basedir/2017/friends/NLOReweighting/"
 
 # Samples Run2018
 ARTUS_OUTPUTS_2018="$basedir/2018/ntuples/"
 SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
-FF_Friends_2018="$basedir/2018/friends/FakeFactors/"
+FF_Friends_2018="$basedir/2018/friends/FakeFactors_v3/"
 NLOReweighting_Friends_2018="$basedir/2018/friends/NLOReweighting/"
 
 
 # ERA handling
-if [[ $ERA == *"2016"* ]]
+if [[ $E == *"2016"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2016
     SVFit_Friends=$SVFit_Friends_2016
     FF_Friends=$FF_Friends_2016
     NLOReweighting_Friends=$NLOReweighting_Friends_2016
-elif [[ $ERA == *"2017"* ]]
+elif [[ $E == *"2017"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2017
     SVFit_Friends=$SVFit_Friends_2017
     FF_Friends=$FF_Friends_2017
     NLOReweighting_Friends=$NLOReweighting_Friends_2017
-elif [[ $ERA == *"2018"* ]]
+elif [[ $E == *"2018"* ]]
 then
     ARTUS_OUTPUTS=$ARTUS_OUTPUTS_2018
     SVFit_Friends=$SVFit_Friends_2018
