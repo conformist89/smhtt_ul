f2a5012ca3f4edbbff9634afa5ecca9e6a114670
Maximilian Burkart <maxiburkart@gmail.com>
2021-05-23 22:14:45 +0200

    Switch to new root version.

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt/setup.sh
+source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc10-opt/setup.sh
 
 export PYTHONPATH=$PWD:$PYTHONPATH
