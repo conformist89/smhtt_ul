7ad9ce81018f4dc873fb5717d6df94e995094a12
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-06-30 13:27:15 +0200

    use lcg 102

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc10-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102rc1/x86_64-centos7-gcc11-opt/setup.sh
 
 export PYTHONPATH=$PWD:$PYTHONPATH
