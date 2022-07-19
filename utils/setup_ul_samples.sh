2b2cdaf3b860b02c76772939fafb0d06c2dd918c
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-07-19 11:04:50 +0200

    consistent var naming

--- utils/setup_ul_samples.sh
+++ utils/setup_ul_samples.sh
@@ -18,7 +18,7 @@ NTUPLES_2017="$basedir"
 
 # Samples Run2018
 NTUPLES_2018="$basedir"
-xsec_friends_2018="/ceph/sbrommer/smhtt_ul/${NTUPLETAG}/friends/xsec"
+XSEC_FRIENDS_2018="/ceph/sbrommer/smhtt_ul/${NTUPLETAG}/friends/xsec"
 # SVFit_Friends_2018="$basedir/2018/friends/SVFit/"
 # FF_Friends_2018="$basedir/2018/friends/FakeFactors_v5/"
 # NLOReweighting_Friends_2018="$basedir/2018/friends/NLOReweighting/"
@@ -40,7 +40,7 @@ then
 elif [[ $ERA == *"2018"* ]]
 then
     NTUPLES=$NTUPLES_2018
-    XSEC_FRIENDS=$xsec_friends_2018
+    XSEC_FRIENDS=$XSEC_FRIENDS_2018
     # SVFit_Friends=$SVFit_Friends_2018
     # FF_Friends=$FF_Friends_2018
     # NLOReweighting_Friends=$NLOReweighting_Friends_2018
