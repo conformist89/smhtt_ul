9eefa9328e265c09e8e4ca2c685090d8407f7812
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-08-01 13:35:57 +0200

    update binning to be a much shorter file

--- first_control_plots_ul2018.sh
+++ first_control_plots_ul2018.sh
@@ -5,7 +5,7 @@ ERA=$2
 NTUPLETAG=$3
 TAG=$4
 
-VARIABLES="pt_1,pt_2,eta_1,eta_2,m_vis,jpt_1,jpt_2,jeta_1,jeta_2,mjj,njets,nbtag,bpt_1,bpt_2,mt_1,mt_2,pt_tt,met,pzetamissvis,iso_1"
+VARIABLES="pt_1,pt_2,eta_1,eta_2,m_vis,jpt_1,jpt_2,jeta_1,jeta_2,mjj,njets,nbtag,bpt_1,bpt_2,mt_1,mt_2,pt_tt,pt_tt_pf,iso_1,pfmet,mt_1_pf,mt_2_pf,met,pzetamissvis,pzetamissvis_pf,metphi,pfmetphi"
 
 ulimit -s unlimited
 source utils/setup_root.sh
