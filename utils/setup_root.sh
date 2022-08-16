40c4edbd2302c7f0fc228b08007d2bf1c58f60e2
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-08-16 12:30:19 +0200

    switch to 102 non-rc version

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102rc1/x86_64-centos7-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
 
 export PYTHONPATH=$PWD:$PYTHONPATH
