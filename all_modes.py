62d0f4b45086805ead51ad26671ee355ede73af3
Olha Lavoryk <olhalavoryk@gmail.com>
2022-11-21 10:45:11 +0100

    VS electron discriminator

--- all_modes.py
+++ all_modes.py
@@ -4,8 +4,9 @@ import argparse
 pre_shape_modes = ["CONTROL", "CONTROLREGION", "CONDOR"]
 
 post_shape_modes = ["MERGE", "SYNC", "DATACARD", "FIT",
-"POSTFIT", "PLOT-POSTFIT", "PLOT-SF", "IMPACTS", "JSON"
+"POSTFIT", "PLOT-POSTFIT", "PLOT-SF", "JSON"
 ]
+
 
 # CHANNEL=" mt "
 # ERA=" 2018 "
@@ -18,6 +19,11 @@ parser.add_argument("--channel", type=str, default="mt", help="mt, et, em , tt")
 parser.add_argument("--ntupletag", type=str, default="2022_07_v6", help="UL ntuples version")
 parser.add_argument("--tag", type=str, default="tight_mt_2018UL", help="user define tag")
 parser.add_argument("--mode", type=str, default="cent", help="pre-shape, post-shape")
+parser.add_argument("--wp", type=str, default="tight", help="working point")
+parser.add_argument("--wp_ele", type=str, default="loose", help="VS ELE discriminator working point")
+parser.add_argument("--dt", type=str, default="2_1", help="Deep Tau version")
+
+
 
 args = parser.parse_args()
 
@@ -27,9 +33,11 @@ if args.mode=="pre-shape":
     mode = pre_shape_modes
 if args.mode=="post-shape":
     mode = post_shape_modes
-else:
-    print("Please provide the correct input")
+elif args.mode != "pre-shape" and args.mode!="post-shape":
+    print("******************Please provide the correct input, mode "+ args.mode+ " does not exist")
 
+
+# print(mode)
 for i in range(len(mode)):
-    os.system("./embeddded_tauID_ul2018.sh "+args.channel+" "+args.era+" "+args.ntupletag+" "+args.tag+" "+mode[i])
+    os.system("./embeddded_tauID_ul2018.sh "+args.channel+" "+args.era+" "+args.ntupletag+" "+args.tag+" "+mode[i]+" "+args.wp+" "+args.wp_ele+" "+args.dt)
     
