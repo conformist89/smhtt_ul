f8d1d1314d672f0854bf946471019a607c922e39
Olha Lavoryk <olhalavoryk@gmail.com>
2023-11-21 11:06:01 +0100

    root afaptations on remote machine

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+if [[ $(hostname) =~ portal1 || $(hostname) =~ centos7 ]]; then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+elif [[ $(hostname) =~ bms ]]; then
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos8-gcc11-opt/setup.sh
+else
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+fi
 
 export PYTHONPATH=$PWD:$PYTHONPATH
