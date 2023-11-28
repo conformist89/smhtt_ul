59f20d91e937ff34e38cf59fbddf5a3473399132
Olha Lavoryk <olhalavoryk@gmail.com>
2023-11-28 10:12:45 +0100

    Some 2016 tauid adaptations

--- config/shapes/channel_selection.py
+++ config/shapes/channel_selection.py
@@ -16,14 +16,21 @@ def channel_selection(channel, era, special=None):
                 [
                     ("id_tau_vsMu_Tight_2>0.5", "againstMuonDiscriminator"),
                     ("id_tau_vsEle_VVLoose_2>0.5", "againstElectronDiscriminator"),
-                    ("id_tau_vsJet_Tight_2>0.5", "tau_iso"),
+                    ("id_tau_vsJet_Medium_2>0.5", "tau_iso"),
                     ("iso_1<0.15", "muon_iso"),
                     ("mt_1 < 70", "mt_cut"),
                 ]
             )
             #  Add era specific cuts. This is basically restricted to trigger selections.
             # TODO add 2017 and 2016
-            if era == "2017":
+            if era == "2016preVFP" or era == "2016postVFP":
+                cuts.append(
+                    (
+                        "pt_2>20 && pt_1>=23 && ((trg_single_mu22 == 1) || (trg_single_mu22_tk == 1)  || (trg_single_mu22_eta2p1 == 1)  || (trg_single_mu22_tk_eta2p1 == 1))",
+                        "trg_selection",
+                    ),
+                )
+            elif era == "2017":
                 cuts.append(
                     (
                         "pt_2>30 && (pt_1>=28 && (trg_single_mu27 == 1))",
@@ -214,7 +221,7 @@ def channel_selection(channel, era, special=None):
                 [
                     ("id_tau_vsMu_Tight_2>0.5", "againstMuonDiscriminator"),
                     ("id_tau_vsEle_VLoose_2>0.5", "againstElectronDiscriminator"),
-                    ("id_tau_vsJet_Tight_2>0.5", "tau_iso"),
+                    ("id_tau_vsJet_Medium_2>0.5", "tau_iso"),
                     ("iso_1<0.15", "muon_iso"),
                     ("pzetamissvis > -25", "pzetamissvis"),
                     ("mt_1 < 60", "mt_1"),
@@ -224,6 +231,13 @@ def channel_selection(channel, era, special=None):
                 cuts.append(
                     (
                         "pt_2>20 && pt_1>=28 && ((trg_single_mu27 == 1) || (trg_single_mu24 == 1))",
+                        "trg_selection",
+                    ),
+                )
+            elif era == "2016preVFP" or era == "2016postVFP":
+                cuts.append(
+                    (
+                        "pt_2>20 && pt_1>=23 && ((trg_single_mu22 == 1) || (trg_single_mu22_tk == 1)  || (trg_single_mu22_eta2p1 == 1)  || (trg_single_mu22_tk_eta2p1 == 1))",
                         "trg_selection",
                     ),
                 )
