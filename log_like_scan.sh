f979a85de6d352fc205b5e672f757f6ff0f074d2
Olha Lavoryk <olhalavoryk@gmail.com>
2023-12-14 14:23:09 +0100

    log scan modified

--- log_like_scan.sh
+++ log_like_scan.sh
@@ -8,29 +8,33 @@ CHANNEL=$4
 WP=$5
 WP_ELE=$6
 
-categories=("Pt25to30")
+categories=(Pt25to30)
 
 
-# categories=("Pt20to25" "Pt25to30" "Pt30to35" "Pt35to40" "PtGt40" "DM0" "DM1" "DM10_11" "Inclusive")
+# categories=("Pt20to25" "Pt25to30" "Pt30to35" "Pt35to40" "PtGt40" )
 
 
-tag_folder="simultaneous_fit"
+tag_folder="muonID_uncorr"
 
 
-poi=prop_binhtt_mt_1_Run2018_bin14
+# poi=prop_binhtt_mt_1_Run2018_bin14
 
 
 
 for cat in ${categories[@]}; do
 
+    poi=r_EMB_${cat}
+    
+
     if [[ ! -d lscan_/${tag_folder}/${ERA}/${CHANNEL}/${TAG}/${cat}/${poi} ]]
     then
-        mkdir -p lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}
-        mkdir -p lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}
+        # mkdir -p lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}
+        mkdir -p "lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}"
     fi
 
-    # DATACARD="/work/olavoryk/sim_fit/smhtt_ul/output/datacards/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}/htt_mt_${cat}/"
-    # workspace="/work/olavoryk/sim_fit/smhtt_ul/output/datacards/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}/htt_mt_${cat}/workspace.root"
+    # ls lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}/
+
+    # echo ${poi}
 
     DATACARD="/work/olavoryk/sim_fit/smhtt_ul/output/datacards/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}/cmb/"
     workspace="/work/olavoryk/sim_fit/smhtt_ul/output/datacards/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}/cmb/out_multidim.root"
@@ -41,8 +45,8 @@ for cat in ${categories[@]}; do
     
     combine -M MultiDimFit -n .nominal_${poi} --algo grid --redefineSignalPOIs r_EMB_${cat} -P $poi --floatOtherPOIs 1 --saveInactivePOI 1 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy 0  --setParameterRanges $poi=-2,2 -m 125 -d $workspace --setParameters $poi=0,r=1.00 -v 2 --points 100 
     
-    plot1DScan.py  higgsCombine.nominal_${poi}.MultiDimFit.mH125.root --POI $poi --y-max 16 --output ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}
-    mv ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}*  /work/olavoryk/sim_fit/smhtt_ul/lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}/
+    plot1DScan.py  higgsCombine.nominal_${poi}.MultiDimFit.mH125.root --POI $poi --y-max 6 --output ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}
+    mv ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}*  lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${cat}/${poi}/
 
 done
 exit 0
